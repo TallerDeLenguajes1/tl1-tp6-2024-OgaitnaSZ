951c1ce3dd7382508798c3664db0d2dc3a9f5cb3
Santiago Z <test@gmail.com>
2024-05-21 11:13:15 -0300

    Calculadora 2 en proceso

--- Program.cs
+++ Program.cs
@@ -78,3 +78,38 @@ do{
         }
     }
 }while(bandera==1);
+
+
+//v2
+Console.WriteLine("Ingrese un numero:");
+string buff1= Console.ReadLine();
+if (int.TryParse(buff1, out num1)){
+    //Valor Absoluto
+    double valorAbsoluto = Math.Abs(num1);
+    Console.WriteLine("Valor absoluto: "+valorAbsoluto);
+
+    // Cuadrado
+    double cuadrado = Math.Pow(num1, 2);
+    Console.WriteLine("Cuadrado: " +cuadrado);
+
+    // Raíz cuadrada
+    if (num1 >= 0){
+        double raiz = Math.Sqrt(num1);
+        Console.WriteLine("Raiz cuadrada: "+raiz);
+    }else{
+        Console.WriteLine("No se puede calcular la raíz cuadrada de un número negativo.");
+    }
+
+    // Seno
+    double seno = Math.Sin(num1);
+    Console.WriteLine("Seno: "+seno);
+
+    // Coseno
+    double coseno = Math.Cos(num1);
+    Console.WriteLine("Coseno: "+ coseno);
+
+    // Parte entera de un tipo float
+        int integerPart = (int)num1;
+        Console.WriteLine($"Parte entera: "+integerPart);
+
+}
