9b7cbd4ed2d144c052e17ad18866efc0a1b65042
Santiago Z <test@gmail.com>
2024-05-21 11:15:19 -0300

    Correccion Calculadora V2

--- Program.cs
+++ Program.cs
@@ -26,3 +26,35 @@ if (entradaNum){
 }
 
 //Calculadora V2
+Console.WriteLine("Ingrese un numero:");
+string buff1= Console.ReadLine();
+if (int.TryParse(buff1, out num1)){
+    //Valor Absoluto
+    double valorAbsoluto = Math.Abs(num1);
+    Console.WriteLine("Valor absoluto: "+valorAbsoluto);
+
+    // Cuadrado
+    double cuadrado = Math.Pow(num1, 2);
+    Console.WriteLine("Cuadrado: " +cuadrado);
+
+    // Raíz cuadrada
+    if (num1 >= 0){
+        double raiz = Math.Sqrt(num1);
+        Console.WriteLine("Raiz cuadrada: "+raiz);
+    }else{
+        Console.WriteLine("No se puede calcular la raíz cuadrada de un número negativo.");
+    }
+
+    // Seno
+    double seno = Math.Sin(num1);
+    Console.WriteLine("Seno: "+seno);
+
+    // Coseno
+    double coseno = Math.Cos(num1);
+    Console.WriteLine("Coseno: "+ coseno);
+
+    // Parte entera de un tipo float
+        int integerPart = (int)num1;
+        Console.WriteLine($"Parte entera: "+integerPart);
+
+}
