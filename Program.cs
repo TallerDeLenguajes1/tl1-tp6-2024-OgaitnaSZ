9f55d9e52e6dc96ea82d34b18c7686884dae3b4e
Santiago Z <test@gmail.com>
2024-05-21 10:25:56 -0300

    Ejercicio 1 terminado

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, World!");
+
+int a;
+int b;
+
+a=10;
+b=a;
+
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+
+//Ejercicio 1
+Console.WriteLine("Ingrese un numero:");
+string entrada = Console.ReadLine(); 
+bool entradaNum = int.TryParse(entrada, out int num);
+
+if (entradaNum){
+    int invertido = 0;
+    while (num > 0){
+        int dig = num % 10;
+        invertido = (invertido * 10) + dig;
+        num = num / 10;
+    }
+    Console.WriteLine("El numero invertido es:" + invertido);
+}
